7cbd814c3a112dc369f6b950e7bd6c24fa9b177f
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-08 16:12:36 -0700

    Multulingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,6 @@
  * Return -Always 0 (success)
  */
 int main (void)
-{
-	put("\"Programming is like building a 
-			a multilingual puzzle");
+{	put("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
