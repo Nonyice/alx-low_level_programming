cb912f6cdce91df27494c5e9e98175574f451157
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-09 14:11:09 -0700

    Random Numbers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/*betty style doc for function main*/
+int main(void)
+{
+	int	n;
+	srand(time(0));
+	n= rand() - RAND_MAX/2;
+	printf("%d\n", n);
+		/**
+		 * main - Entry point
+		 * Return: (0)
+		 */
+	/*To compare if number is positve or negative*/
+
+	if(n>0)
+	{
+		printf("n is positive");
+	}
+	else if(n<0)
+	
+	{
+		printf("n is negative");
+	}
+	else 
+	{	printf("n is zero");
+	}
+}
