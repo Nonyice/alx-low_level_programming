852100a94a1fb906ad9132caeff3fd1c26ad2da3
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-09 01:00:16 -0700

    Piece od art

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
  * main-prints the entry point in printf
  * Description:uses the main function to write 
  * a string
- * Return:- Always 0(success)
+ * Return: Always 0(success)
  */
 int	main(void)
 {	printf("with proper grammar, but the outcome is a piece of art,\n")
-return: (0);
+return (0);
 }
