f01c337a9ffdc472e615cf59957c758819b922b8
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-08 17:56:10 -0700

    Multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /*
- * main	-	Entry point
+ * main:	-	Entry point
  * Return:	-	Always	0(success)
  */
 int main(void)
-/* Initializes to zero */
 {	puts("\"Programming is like building a multilingual puzzle");
 	return	(0);
 }
