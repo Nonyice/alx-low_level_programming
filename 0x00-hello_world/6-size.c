8b7a852255588554892fcde11064878613398257
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-09 04:22:16 -0700

    Size of type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
-int main() {
+int main(void)
+	{
 	char charType;
 	int intType;
 	long int longintType;
 	long long int longlongintType;
 	float floatType;
-/* sizeof evaluates the size of types on this computer*/
+/**
+ *  sizeof evaluates the size of types on this computer
+ * Description:uses sizeof to print size of type
+ * Return: 0(success)
+ * */
 	printf("size of a char: %zu bytes\n", sizeof(charType));
 	printf("size of an int: %zu bytes\n", sizeof(intType));
 	printf("size of a long int: %zu bytes\n", sizeof(longintType));
@@ -14,4 +19,4 @@ int main() {
 	printf("size of a float: %zu bytes\n", sizeof(floatType));
 
 	return (0);
-}
+	}
