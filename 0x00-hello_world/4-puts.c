130352a656a82cf214708677c9ecaac8b7a3bf51
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-08 19:13:06 -0700

    Multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main-prints the string in the puts function
- * Description: using the main function, this program prints "programming is like building a multilingual puzzle"
+ * Description: using the main function, this program prints
+ * "programming is like building a multilingual puzzle"
  * Return:	Always	0(success)
  */
 int main(void)
