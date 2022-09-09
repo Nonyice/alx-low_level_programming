e5b7d17cfce1e74658e9b8dca6b27266a2426e29
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-09 01:29:13 -0700

    Piece of art

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include	<stdio.h>
 /**
- * main-prints the entry point in printf
- * Description:uses the main function to write 
- * a string
- * Return: Always 0(success)
+* main - Entry point
+* Return: Always 0 (success)
  */
-int	main(void)
-{	printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+int main(void)
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
