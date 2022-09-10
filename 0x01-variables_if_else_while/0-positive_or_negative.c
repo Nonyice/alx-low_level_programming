49e2de18cd3ada089890ec8ab925b76419c55ca1
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-10 09:58:24 -0700

    Random

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,8 +15,10 @@ int main(void)
 	if (n == 0)
 		printf("%d is zero\n", n);
 	else if (n < 0)
-		printf("%d is negative\n", n);
+		printf("%d is negative\n",);
 	else
-	       	printf("%d	is	positive\n",);
+		printf("%d is positive\n",);
 	return (0);
 }
+	
+
