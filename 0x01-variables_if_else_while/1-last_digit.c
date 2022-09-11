31acab76406abdb13d4ba2951a5fe36a2fc57a46
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-11 03:45:18 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  * Description: RAND_MAX
@@ -7,7 +8,7 @@
  */
 int main(void)
 {
-	int n, digit;
+	int n;
 	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
