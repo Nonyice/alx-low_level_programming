f81b7d126247a9be398bc312a7cb06b9ed48842b
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-11 07:03:15 -0700

    Letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Descrption: Print alphaBets
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	char letter;
+
+	for (letter == 'a'; letter <= 'z'; letter++)
+	{	if (letter != 'e' && letter != 'q')
+		putchar(letter);
+	}
+		putchar('\n');
+	return (0);
+}
