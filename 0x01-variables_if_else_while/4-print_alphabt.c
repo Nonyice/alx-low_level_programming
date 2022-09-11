83b09c3bf4082696028bd4ebf709ab72f1af39e0
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-11 07:22:55 -0700

    letter

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,8 +7,8 @@
 int main(void)
 {	char letter;
 
-	for (letter == 'a'; letter <= 'z'; letter++)
-	{	if (letter != 'e' && letter != 'q')
+	for (letter == 'a'; letter <= 'z'; letter != 'e'; letter != 'q'; letter++)
+	{ 
 		putchar(letter);
 	}
 		putchar('\n');
