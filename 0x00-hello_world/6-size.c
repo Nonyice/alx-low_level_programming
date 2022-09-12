2f07b395a2ee835816fce21efe6a3fc14eadd54f
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-12 04:09:49 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,11 @@ int main(void)
 	long long int longlongintType;
 	float floatType;
 /**
-*main - print size of type using the msion functiopn sizeof
- * Description:char type in byte(s)
- * Return 0 (success)
+ * main - print size of type
+ * Description:sizeof type and byte(s)
+ * Return: Always 0 (Success)
  */
+
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
