053a7abbb532e6b0c56a25685d627c6ab7bd9192
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-12 06:16:19 -0700

    sizs

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-/*program to print size of type*/
+/**
+ * main - Entry point
+ * Descripion: print size of type
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {	char charType;
 	int intType;
 	long int longintType;
 	long long int longlongintType;
 	float floatType;
-/**
- * main - print size of type
- * Description:sizeof type and byte(s)
- * Return: Always 0 (Success)
- */	
 		
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-
 	return (0);
 }
