49df271f5475b4e6c7b77bd75881ddc8078d5733
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-12 03:31:39 -0700

    comp3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,20 +7,20 @@
 int main(void)
 {	int c, i;||
 
-	{	for (c = '0'; c <= '9'; c++)
-		for (i ='0'; i <= '9'; i++)
-	}
+	for (c = '0'; c <= '9'; c++)
+	{for (i ='0'; i <= '9'; i++)
 		{if (c < i)
-			{putchar(c);
+			{
+			putchar(c);
 			putchar(i);
 			}
-			{	if (c != '8' || (c = '8' &&!= '9'))}
-
-
-		{putchar(',');
-		putchar(' ');
-		}
-	}
-		putchar('\n');
+	if (c != '8' || (c = '8' &&!= '9'))
+			{
+			putchar(',');	
+			putchar(' ');
+			}
+			putchar('\n');
 	return (0);		
+}
+}
 }
