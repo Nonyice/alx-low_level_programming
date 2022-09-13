6dd618de268507f9410aeedcfff41261156eb438
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-13 12:15:32 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include <main.h>
 /**
- * main - pri _putchar
- *
- * Return: Always 0 (Success)
+ * main - print putchar
+ * Return:Always 0 (Success)
  */
 int main(void)
-{
-	char str[] = "_putchar";
-
-	{
-	printf("_putchar\n");
-	}
+{	putchar('');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
-
