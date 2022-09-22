54e77de885158bebdbd3506c3f3de4e5a2beb99f
Emmanuel <emmanuelcibe@yahoo.com>
2022-09-22 16:51:16 -0700

    Cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,32 @@
 #include "main.h"
 /**
- *  *cap_string - capitalizes every first letter of a word in a string.
- *   *separators of words are:  space, tabulation,
- * new line, ,, ;, ., !, ?, ", (, ), {, and }.
- *@s: pointer to string.
+ * cap_string - capitalizes all words in a string
+ * @s: string to capitalize
  *
- *Return: pointer to s.
+ * Return: address of s
  */
 char *cap_string(char *s)
 {
-		int count;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-		/*  scan through string */
-		count = 0;
-		while (s[count] != '\0')
-		{/* if next character after count is a char , capitalise it */
-		if (s[0] >= 97 && s[0] <= 122)
-		{
-		s[0] = s[0] - 32;
-		}
-		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
-		|| s[count] == ',' || s[count] == ';' || s[count] == '.'
-	    	|| s[count] == '.' || s[count] == '!' || s[count] == '?'
-	    	|| s[count] == '"' || s[count] == '(' || s[count] == ')'
-	      	|| s[count] == '{' || s[count] == '}')
-		{
-		if (s[count + 1] >= 97 && s[count + 1] <= 122)
-		{
-		s[count + 1] = s[count + 1] - 32;
-		}
-		}
-		count++;
-		}
-		return (s);
+	while (*(s + i))
+	{
+	if (*(s + i) >= 'a' && *(s + i) <= 'z')
+	{
+	if (i == 0)
+	*(s + i) -= 'a' - 'A';
+	else
+	{
+	for (j = 0; j <= 12; j++)
+	{
+	if (a[j] == *(s + i - 1))
+	*(s + i) -= 'a' - 'A';
+	}
+	}
+	}
+	i++
+	}
+	return (s);
 }
 
-	
