dd860f28a23a0492a0aac6ada8df9222ceec2b05
Emmanuel <emmanuelcibe@yahoo.com>
2022-12-09 19:31:25 -0800

    task add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,8 +11,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 	dlistint_t *h;
-	new = malloc(sizeof(dlistint_t));
 
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	return (NULL);
 	new->n = n;
