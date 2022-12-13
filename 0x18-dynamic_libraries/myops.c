2d00865938812a6de616f7bab5ce5ca835e884c0
Emmanuel <emmanuelcibe@yahoo.com>
2022-12-13 11:13:59 -0800

    ctype call from python

---
+++ 0x18-dynamic_libraries/myops.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a - first integer
+ * @b - second integer
+ *
+ * Return: sum of the integers
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	Return: (sum);
+}
+	
+/**
+ * sub - subtracts two integers
+ * @a - first integer
+ * @b - second integer
+ *
+ * Return: the difference of the integers
+ */
+int sub(int a, int b)
+{
+        int sub;
+
+	sub = a - b;
+	Return: (sub);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a - first integer
+ * @b - second integer
+ *
+ * Return: the product of the integers
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+        mul = a * b;
+        Return: (mul);
+}
+
+/**
+ * mod - finds the remainder when two integers are divided
+ * @a - first integer
+ * @b - second integer
+ *
+ * Return: remainder of the integers division
+ */
+int mod(int a, int b)
+{
+	int mod;
+	
+	mod = a % b;
+        Return: (mod);
+}
